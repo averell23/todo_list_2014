f59f04814f9ffdd8156f9dfff0b2629b8cf7d109
Daniel Hahn <dha@betterplace.org>
2014-03-03 11:45:38 +0100

    First controller and route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
+# class TasksController extends ApplicationController
 
+  # public Object index() { return render("something"); }
   def index
     render text: "Hello World"
   end
